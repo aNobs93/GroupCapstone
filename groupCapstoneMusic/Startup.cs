53515898b7a8a6eb4a023f1f041a43fa00128092
Adam Nobs <nobsadam@gmail.com>
2020-01-16 14:43:44 -0600

    Initial creation of asp.net mvc project for group capstone

---
+++ groupCapstoneMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(groupCapstoneMusic.Startup))]
+namespace groupCapstoneMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
